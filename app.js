161d9656cc008a3bae4c3ae22ef9029432be63d5
Kuma <kumagithub@mail.ru>
2023-03-28 13:41:57 +0600

    feat : sending hex-code of colors via the link

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function generateRandomColor() {
   }
   return "#" + color;
 }
+
 // function setRandomColors() {
 //   cols.forEach((col) => {
 //     col.style.background = generateRandomColor();
@@ -38,7 +39,9 @@ function CopyToClickboard(text) {
   navigator.clipboard.writeText(text);
 }
 console.log(navigator.clipboard.writeText());
+
 function setRandomColors() {
+  const colors = [];
   cols.forEach((col) => {
     const isLocked = col.querySelector("i").classList.contains("fa-lock");
     const text = col.querySelector("h2");
@@ -46,18 +49,27 @@ function setRandomColors() {
     const color = chroma.random();
 
     if (isLocked) {
+      colors.push(text.textContent);
       return;
     }
+    colors.push(color);
     text.textContent = color;
     col.style.background = color;
 
     setTextColor(text, color);
     setTextColor(button, color);
   });
+  updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? "black" : "white";
+}
+function updateColorsHash(colors = []) {
+  document.location.hash = colors.map((col) =>{
+    return col.toString().substring(1)
+  })
+  .join('-')
 }
 setRandomColors();
